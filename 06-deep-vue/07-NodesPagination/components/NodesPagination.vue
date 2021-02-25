df91824cb8a34017e02d97ace709f8a095561f9d
Alyona <aljonaart8@yandex.ru>
2021-02-25 17:23:48 +0300

    add solution for 06-deep-vue/07-NodesPagination

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,17 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: Number,
+    perPage: Number,
+  },
+
+  render(h) {
+    let slots = this.$slots.default;
+    return h('div', slots ? slots.filter((el, ind) => {
+      if(Math.ceil((ind + 1) / this.perPage) == this.page) return el}) : null) 
+  },
 };
 </script>
 
