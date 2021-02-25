41a167e72a66a716a5a76dadf33913c960addaae
Alyona <aljonaart8@yandex.ru>
2021-02-25 23:58:41 +0300

    add solution for 08-interactions-and-logic-reuse/01-ListView

--- 08-interactions-and-logic-reuse/01-ListView/components/ListView.vue
+++ 08-interactions-and-logic-reuse/01-ListView/components/ListView.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="meetups-list">
-    <!-- Требуется scoped-slot для каждого элемента списка -->
+    <template v-for="item in items">
+      <slot :item="item" />
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ListView',
+  
+  props: {
+    items: Array,
+  }
 };
 </script>
 
